7291f151de254f02a7b360a129429fbcaf529f2a
Jan Sebetovsky <jan@sebetovsky.cz>
2011-02-26 17:04:13 +0100

    Predelana a rozsirena souborova struktura magisterske casti.

---
+++ nmgr/README - adresarova_struktura.txt
@@ -0,0 +1,11 @@
+U magisterských státnic je situace podobně jednochá jako u matematiky u bakalářských státnic. Takže jsou texty rozděleny do hloubky jednotlivých okruhů.
+Jedinou výjimku tvoří Složitost a vyčíslitelnost, která je částečně sdílená.
+
+V kořeni jsou vytvořené soubory, které slučují všechny otázky pro daný studijní plán. Jednotlivé součásti jsou načítány z adresáře „spolecne“ a adresáře pro odpovídající obor.
+
+Názvy souborů jsem se snažil udělat v podobném duchu jako u bakalářské části, tedy přesný název dané části bez diakritiky s nahrazenými nepovolenými znaky, ale některé otázky byly příliš dlouhé, takže jsem je zkrátil a doplnil třemi tečkami (nejlépe v místě čárky).
+Navíc jsem zavedl pravidlo zkracování podle dvojtečky, tedy pokud se někde v názvu vyskytovala dvojtečka (a text před ní stačil pro rozlišení), tak text za dvojtečkou se již do názvu nepíše (a nenahrazuje trojtečkou). Příklady:
+„Věty o zrychlení a o mezerách, věty o hierarchii tříd složitosti, konstruovatelné funkce, vztahy mezi časovými a prostorovými mírami a determinismem a nedeterminismem, Savitchova věta“ -> „Vety_o_zrychleni_a_o_mezerach...“
+„Metody tvorby algoritmů: rozděl a panuj, dynamické programování, hladový algoritmus“ -> „Metody_tvorby_algoritmu“
+
+Také jsem ponechal konvenci ve velikosti písmen, tedy okruhy, obory, otázky atd. velkými písmeny, ale adresáře vždy malými (bylo to tak i u bakalářské části).
--- nmgr/make.bat
+++ nmgr/make.bat
@@ -1,20 +1,18 @@
 @echo off
 IF not EXIST out\ mkdir out
 
-pdflatex -interaction=nonstopmode -output-directory=out "i1-algoritmy-a-slozitost.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i1-neproceduralni-programovani-a-umela-inteligence.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I1-Algoritmy_a_slozitost.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I1-Neproceduralni_programovani_a_umela_inteligence.tex"
 
-pdflatex -interaction=nonstopmode -output-directory=out "i2-databazove-systemy.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i2-pocitacova-grafika.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i2-softwarove-inzenyrstvi.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i2-spolehlive-systemy.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i2-systemove-architektury.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I2-Databazove_systemy.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I2-Pocitacova_grafika.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I2-Softwarove_inzenyrstvi.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I2-Spolehlive_systemy.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I2-Systemove_architektury.tex"
 
-pdflatex -interaction=nonstopmode -output-directory=out "i3-matematicka-lingvistika-2010.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i3-matematicka-lingvistika.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I3-Matematicka_lingvistika.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I3-Matematicka_lingvistika-2009.tex"
 
-pdflatex -interaction=nonstopmode -output-directory=out "i4-diskretni-matematiika-a-kombinatoricka-optimalizace.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i4-matematicke-struktury-informatiky.tex"
-pdflatex -interaction=nonstopmode -output-directory=out "i4-optimalizace.tex"
-
-rem dvipdfm -o "out/Programovani_1.pdf" "out/Programovani_1.dvi"
+pdflatex -interaction=nonstopmode -output-directory=out "I4-Diskretni_matematika_a_kombinatoricka_optimalizace.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I4-Matematicke_struktury_informatiky.tex"
+pdflatex -interaction=nonstopmode -output-directory=out "I4-Optimalizace.tex"
